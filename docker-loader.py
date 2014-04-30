8ee93a2bf5baeaba730d838bc8c20a3e102aeb4e
Batandwa Colani <i@batandwa.me>
2014-04-30 07:11:41 +0200

    Reading from settings of current directory.

--- docker-loader.py
+++ docker-loader.py
@@ -3,35 +3,36 @@
 import os
 import sys
 import re
+import ConfigParser
 import subprocess
 
-def main(container_name):
+def main(container_name, config):
   if len(sys.argv) == 1:
     print 'No action specified'
     exit(1)
   elif sys.argv[1] == 'stop':
-    stop(container_name)
+    stop(container_name, config)
   elif sys.argv[1] == 'kill':
-    kill(container_name)
+    kill(container_name, config)
   elif sys.argv[1] == 'start':
-    start(container_name)
+    start(container_name, config)
   elif sys.argv[1] == 'restart':
-    restart(container_name)
+    restart(container_name, config)
 
-def stop(container_name):
+def stop(container_name, config):
   print 'Stopping container ' + container_name
   subprocess.call(['sudo', 'docker', 'stop', container_name])
 
-def kill(container_name):
-  stop(container_name)
+def kill(container_name, config):
+  stop(container_name, config)
   print 'Removing container ' + container_name
   subprocess.call(['sudo', 'docker', 'rm', container_name])
 
-def restart(container_name):
-  kill(container_name)
-  start(container_name)
+def restart(container_name, config):
+  kill(container_name, config)
+  start(container_name, config)
 
-def start(container_name):
+def start(container_name, config):
   # print 'Restarting dnsmasq'
   # subprocess.call(['sudo', '/etc/init.d/dnsmasq', 'restart'])
   # print 'Fixing permissions'
@@ -40,9 +41,22 @@ def start(container_name):
   # subprocess.call(['scripts/containerinfo.py'])
   print 'Running container %s' % (container_name, )
   # TODO: Put these in a settings file.
-  subprocess.call(['sudo', 'docker', 'run', '-d', '-v', current_folder_path + '/' + current_folder_name + ':/docker', '-v', '' + current_folder_path + '/' + current_folder_name + '/mysql:/var/lib/mysql', '-v', '' + current_folder_path + '/' + current_folder_name + '/site:/var/www', '-name', container_name, 'fjmk/docker-drupal-dev'])
+  call_parameters = ['sudo', 'docker', 'run']
+  detached = config.getboolean('default', 'detached')
+  if detached:
+    call_parameters.append('-d')
 
-  web_proxy_name = 'web-proxy'
+  for vol in config.items('volumes'):
+    call_parameters.append('-v')
+    call_parameters.append(vol[1].replace('%(dir)', os.getcwd()) + ':' + vol[0].replace('%(dir)', os.getcwd()))
+
+  call_parameters.append('-name')
+  call_parameters.append(config.get('default', 'container name'))
+  call_parameters.append(config.get('default', 'image name'))
+  # print call_parameters
+  subprocess.call(call_parameters)
+
+  web_proxy_name = config.get('default', 'proxy')
   print 'Restarting container %s' % (web_proxy_name, )
   subprocess.call(['sudo', 'docker', 'restart', web_proxy_name])
 
@@ -50,7 +64,13 @@ def start(container_name):
 
 if __name__ == '__main__':
   current_folder_path, current_folder_name = os.path.split(os.getcwd())
-  config = ConfigParser
+  config = ConfigParser.RawConfigParser()
+  config.read(current_folder_path + '/' + current_folder_name + '/docker-loader.conf')
+  # print config.get('default', 'proxy')
+  # print config.items('volumes')
+  # a_float = config.getfloat('a_float')
+  # print config
+
   container_name = re.sub(r'[^a-zA-Z\-_ ]', '', current_folder_name.lower()).replace(' ', '-').replace('--', '-')
-  main(container_name)
+  main(container_name, config)
 
